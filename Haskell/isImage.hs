d9aca1adf64dae20695f66fcfeb3e287a05060ce
nikizhelqzkov <nikizhelqzkov@gmail.com>
2020-12-25 15:09:18 +0200

    adding some functions

--- Haskell/isImage.hs
+++ Haskell/isImage.hs
@@ -1,10 +1,9 @@
 main :: IO ()
 main = do
-  print (isImage [1,2,4,4][1,1,2,1])
+  print (isImage [1,2,3][5,6,7])
 
 isImage :: [Int] -> [Int] -> Bool
-isImage [] [_] = False
-isImage [_] [] = False
+
 isImage xs ys = helper xs ys 0
   where
     helper xs ys count
