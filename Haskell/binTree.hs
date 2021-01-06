41c387d6c50e588e03a43ba12513f235342e039f
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-01-06 18:42:19 +0200

    adding bin tree

---
+++ Haskell/binTree.hs
@@ -0,0 +1,23 @@
+main :: IO ()
+main = do
+  print (countInteresting t1)
+  print (countInteresting t2)
+
+data BTree = Empty | Node Int BTree BTree
+
+countInteresting :: BTree -> Int
+countInteresting Empty = 0
+countInteresting (Node 1 Empty Empty) = 1
+countInteresting (Node 2 treeL Empty) = 1 + countInteresting treeL
+countInteresting (Node 2 Empty treeR) = 1 + countInteresting treeR
+countInteresting (Node 4 (Node valL treeLL treeLR) (Node valR treeRL treeRR)) = 1 + 
+                                                                 countInteresting (Node valL treeLL treeLR) + 
+                                                                 countInteresting (Node valR treeRL treeRR)
+countInteresting (Node _ treeL treeR) = countInteresting treeL + countInteresting treeR
+
+
+t1 :: BTree
+t1 = Node 16 (Node 0 Empty Empty) (Node 4 (Node 1 Empty Empty) (Node 0 Empty Empty))
+
+t2 :: BTree
+t2 = Node 4 (Node 0 Empty Empty) (Node 2 (Node 1 Empty Empty) Empty)
